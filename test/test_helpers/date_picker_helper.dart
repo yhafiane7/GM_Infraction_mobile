4a56f85e8405003476e77797b96f37c7ffebc362
maokli24 <102806508+maokli24@users.noreply.github.com>
2025-09-23 16:32:17 +0100

    Remove unnecessary import from date picker helper file to clean up the codebase.

--- test/test_helpers/date_picker_helper.dart
+++ test/test_helpers/date_picker_helper.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
 
 /// Opens a Material date picker by tapping on [dateFieldFinder]
 /// and confirms the selection (defaults to current displayed date)
